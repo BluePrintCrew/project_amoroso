c3617ab19dbcf57fd6bb52028b7f8bc7763944c5
NOOBJE <guswp320@gmail.com>
2025-01-13 20:35:04 +0900

    add .gitignore , amorosobackend

---
+++ backend/AmorosoBackend/src/main/java/org/example/amorosobackend/AmorosoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.amorosobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorosoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmorosoBackendApplication.class, args);
+    }
+
+}
