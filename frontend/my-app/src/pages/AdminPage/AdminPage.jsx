a457ce23f5bdabe3d00ba6d1d2d041f6e08a6c2c
jhm991231 <jhm991231@ajou.ac.kr>
2025-02-18 15:06:46 +0900

    관리자 페이지에서만 root margin 제거

--- frontend/my-app/src/pages/AdminPage/AdminPage.jsx
+++ frontend/my-app/src/pages/AdminPage/AdminPage.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import AdminSidebar from "../../components/Admin/AdminSidebar/AdminSidebar";
+import React, { useEffect } from "react";
+
 import AdminHeader from "../../components/Admin/AdminHeader/AdminHeader";
-import "./AdminPage.css";
+import AdminSidebar from "../../components/Admin/AdminSidebar/AdminSidebar";
+import styles from "./AdminPage.module.css";
 
 function AdminPage() {
+  useEffect(()=>{
+    const rootElement=document.getElementById("root");
+    const originalMargin=rootElement.style.margin;
+
+    rootElement.style.margin="0";
+    rootElement.style.padding="0";
+
+    return()=>{
+      rootElement.style.margin=originalMargin;
+    };
+  },[])
+  
   return (
-    <div className="admin-page-container">
+    <div className={styles.AdminPage}>
      {/* <AdminHeader /> */}
       <AdminSidebar />
-      <div className="admin-page-content">
-        {/* ... */}
       </div>
-    </div>
   );
 }
 
